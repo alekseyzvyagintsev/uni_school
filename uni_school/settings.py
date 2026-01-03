264ec879e0e7f92f341db2107b0b72be95b0b894
Aleksey Zvyagintsev <alex0236889@gmail.com>
2026-01-03 22:34:31 +0300

    Для работы с документацией проекта выбрана и настроена библиотека  drf-spectacular.

--- uni_school/settings.py
+++ uni_school/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django_filters",
     "rest_framework",
     "rest_framework_simplejwt",
+    'drf_spectacular',
     "materials",
     "users",
 ]
@@ -41,6 +42,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 # Настройки срока действия токенов
--- uni_school/urls.py
+++ uni_school/urls.py
@@ -2,12 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("admindocs/", include("django.contrib.admindocs.urls")),
     path("", include("materials.urls", namespace="materials")),
     path("", include("users.urls", namespace="users")),
+    # Схема OpenAPI 3
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Swagger UI
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
 
 if settings.DEBUG:
