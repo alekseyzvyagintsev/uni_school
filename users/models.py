f76687a8fdb85c1d2cac7a28dad10374b84b26cb
Aleksey Zvyagintsev <alex0236889@gmail.com>
2026-01-05 16:28:23 +0300

    Удалены поля необходимые для хранения не JWT токенов и времени их жизни

--- users/models.py
+++ users/models.py
@@ -39,8 +39,6 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
     country = models.CharField(max_length=50, blank=True, null=True)
     is_active = models.BooleanField(default=False)
-    activation_token = models.CharField(max_length=100, blank=True, null=True)
-    token_expires_at = models.DateTimeField(blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = [
