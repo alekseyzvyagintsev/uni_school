6b345368b66fa3dddcb4c7861352fc51bd4586ab
Aleksey Zvyagintsev <alex0236889@gmail.com>
2026-01-13 21:30:22 +0300

    Добавлен докстринг к задаче notify_subscribers_on_course_update()

--- users/tasks.py
+++ users/tasks.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 
 @shared_task
 def notify_subscribers_on_course_update_task():
+    """Celery задача для уведомления подписчиков о новых курсах."""
     try:
         notify_subscribers_on_course_update()
         return "Уведомления о курсах отправлены"
